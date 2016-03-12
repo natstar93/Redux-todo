396ad0633f5b671083ab5cba6d4311b9e7f1da39
Natalie Akam <natalie.akam@gmail.com>
2016-03-12 11:52:07 +0000

    Adds testing framework and babel

---
+++ test/actions.spec.js
@@ -0,0 +1,9 @@
+import chai from 'chai';
+const expect = chai.expect;
+
+describe('actions', () => {
+  it('creates action to add a todo', () => {
+    const todoName = 'Test with Mocha';
+    expect(1).to.equal(1);
+  });
+});
